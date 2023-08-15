a46e6c7cdaa09a2d67fce6c670ef09771952b7cc
Olexie <116206464+Olexie@users.noreply.github.com>
2023-08-15 22:51:12 +0100

    added task 2.5 folder

--- source/books/models.py
+++ source/books/models.py
@@ -21,6 +21,7 @@ class Book(models.Model):
     price = models.FloatField(help_text='in US dollars $')
     genre = models.CharField(max_length=12, choices=genre_choices, default='cl')
     book_type = models.CharField(max_length=12, choices=book_type_choices, default='hc')
+    pic = models.ImageField(upload_to='books', default='no_picture.jpg')
 
     def __str__(self):
         return str(self.name)
